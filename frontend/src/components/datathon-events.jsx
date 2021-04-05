6097256e24abb88ac9aba67244b694e9f04e931f
dilan-js <dilannana.js@gmail.com>
2021-04-04 21:06:58 -0500

    adding UI details

--- frontend/src/components/datathon-events.jsx
+++ frontend/src/components/datathon-events.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import { Container, Row, Col } from "react-bootstrap";
+import styled from "styled-components";
 import devpost from "../Images/devpost.svg";
 import slack from "../Images/slack.svg";
 import drive from "../Images/drive.svg";
@@ -10,6 +11,7 @@ import api from "../global/api.js";
 
 function Links() {
   const [slackNotifs, setSlackNotifs] = useState([]);
+  const [selectedDate, setSelectedDate] = useState("");
   const dates = [
     {
       date: "March 20",
@@ -76,45 +78,35 @@ function Links() {
       .catch((err) => console.log(err));
   };
 
+  const toggleDateSelect = (date) => {
+    setSelectedDate(date);
+  };
+
   useEffect(() => {
     retrieveSlackAnnouncements();
   }, []);
 
+  console.log(selectedDate);
   return (
     <div id="link-section">
       <h2 class="section-heading">Datathon Events</h2>
-      <Container style={{ backgroundColor: "red" }}>
+      <Container style={{}}>
         <Row style={{ display: "flex", justifyContent: "center" }}>
-          <Col xs={12} md={6} style={{ height: 600 }}>
-            <div
-              style={{
-                fontWeight: "bold",
-                color: "yellow",
-                fontSize: 30,
-              }}
-            >
-              Announcements
-            </div>
-            <div
-              style={{
-                height: "100%",
-                backgroundColor: "#E1F4FD",
-                borderRadius: 20,
-                paddingBottom: 20,
-                overflow: "scroll",
-              }}
-            >
-              This is for slack
+          <Col xs={12} md={6}>
+            <MainTitles>Announcements</MainTitles>
+            <AnnouncementContainer>
               {slackNotifs.map((notif) => {
                 return (
-                  <div>
-                    {notif.text}
-                    <br />
-                    {dayjs(notif.timestamp * 1000).format("MM/DD/YYYY")}
-                  </div>
+                  <SlackNotif>
+                    <AnnouncementTitle>Announcement</AnnouncementTitle>
+                    <SlackDateDisplay>
+                      {dayjs(notif.timestamp * 1000).format("MM/DD/YYYY")}
+                    </SlackDateDisplay>
+                    <SlackNotifDescription>{notif.text}</SlackNotifDescription>
+                  </SlackNotif>
                 );
               })}
-            </div>
+            </AnnouncementContainer>
           </Col>
           <Col
             xs={12}
@@ -126,68 +118,43 @@ function Links() {
               overflow: "hidden",
             }}
           >
-            <div
-              style={{
-                fontWeight: "bold",
-                color: "yellow",
-                fontSize: 30,
-              }}
-            >
-              Upcoming Events
-            </div>
+            <MainTitles>Upcoming Events</MainTitles>
 
             <div style={{ overflow: "scroll", flex: 1 }}>
               <Row
                 style={{
                   display: "flex",
                   justifyContent: "flex-start",
-                  padding: 20,
+                  padding: "0px 20px",
                 }}
               >
                 {dates.map((date) => {
                   return (
-                    <div
-                      style={{
-                        padding: "10px 20px",
-                        backgroundColor: "yellow",
-                        marginRight: 10,
-                        borderRadius: 3,
-                        marginTop: 10,
-                      }}
+                    <DateCard
+                      selected={selectedDate.length > 0}
+                      onClick={() => toggleDateSelect(date.date)}
                     >
                       {date.date}
-                    </div>
+                    </DateCard>
                   );
                 })}
               </Row>
-              <div
-                style={{
-                  padding: "10px 5px",
-                  borderRadius: 10,
-                  overflowY: "scroll",
-                }}
-              >
+              <EventCardContainer>
                 {events.map((event) => {
                   return (
                     <>
-                      <div
+                      <EventCard
                         key={event.id}
-                        style={{
-                          backgroundColor: "#f4f4f4",
-                          padding: "10px 15px",
-                          margin: 0,
-                          borderRadius: 10,
-                          marginBottom: 10,
-                        }}
+                        onClick={() => console.log("render dialog here")}
                       >
                         <div>{event.title}</div>
                         <div>{event.date}</div>
                         <div>{event.time}</div>
-                      </div>
+                      </EventCard>
                     </>
                   );
                 })}
-              </div>
+              </EventCardContainer>
             </div>
           </Col>
         </Row>
@@ -197,3 +164,82 @@ function Links() {
 }
 
 export default Links;
+
+const MainTitles = styled.div`
+  color: rgba(102, 198, 177, 1);
+  line-height: 100%;
+  font-size: 30px;
+  font-weight: bold;
+  font-style: normal;
+  font-family: Sora;
+  margin-bottom: 30px;
+`;
+
+const AnnouncementContainer = styled.div`
+  flex: 1;
+  min-height: 92%;
+  background-color: #e1f4fd;
+  border-radius: 20px;
+  padding-bottom: 20px;
+  overflow: scroll;
+  margin-bottom: 20px;
+`;
+
+const AnnouncementTitle = styled.h1`
+  font-size: 20px;
+  line-height: 25px;
+  font-weight: bold;
+  font-style: normal;
+  font-family: Sora;
+  color: rgba(0, 43, 82, 1);
+  margin-bottom: 3px;
+`;
+
+const SlackNotif = styled.div`
+  padding-left: 30px;
+  padding-bottom: 15px;
+  padding-top: 15px;
+`;
+
+const SlackDateDisplay = styled.div`
+  color: rgba(28, 117, 188, 1);
+  line-height: 25px;
+  font-size: 16px;
+  font-weight: normal;
+  font-style: normal;
+  padding: 0px;
+  margin: 5px 0px;
+`;
+
+const SlackNotifDescription = styled.div`
+  font-size: 16px;
+  color: rgba(0, 0, 0, 1);
+`;
+
+const DateCard = styled.div`
+  padding: 10px 20px;
+  border: 1px solid #e1f4fd;
+  color: #e1f4fd;
+  margin-right: 10px;
+  border-radius: 3px;
+  margin-bottom: 10px;
+  ${({ selected }) => {
+    return `
+    color: rgba(28, 117, 188, 1);
+    background-color: rgba(225,244,253,1);
+   `;
+  }};
+`;
+const EventCard = styled.div`
+  background-color: #f4f4f4;
+  padding: 10px 15px;
+  margin: 0px;
+  border-radius: 10px;
+  margin-bottom: 10px;
+`;
+
+const EventCardContainer = styled.div`
+  padding: 10px 5px;
+  border-radius: 10px;
+  overflow-y: scroll;
+`;
